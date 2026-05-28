a27d81136ef34320a123fb00f970a88620ffda2b
Tung Anh Hoang <tunganhcb2k2@gmail.com>
2026-05-29 00:36:01 +0700

    Header+Footer

---
+++ ShopeeClone/src/components/Footer/Footer.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='py-16 bg-neutral-100'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-4'>
+          <div className='lg:col-span-1'>© 2026 Shopee. Tất cả các quyền được bảo lưu.</div>
+        </div>
+        <div className='lg:col-span-2'>
+          <div>
+            Quốc gia & Khu vực: Argentina Singapore Indonesia Thái Lan Malaysia Việt Nam Philippines Brazil México Đài
+            Loan
+          </div>
+        </div>
+        <div className='text-center text-sm mt-10'>
+          <div>Công ty TNHH Shopee</div>
+          <div className='mt-2'>
+            Địa chỉ: Tầng 4-5-6, Tòa nhà Capital Place, số 29 đường Liễu Giai, Phường Ngọc Hà, Thành phố Hà Nội, Việt
+            Nam
+          </div>
+          <div className='mt-2'>
+            Chăm sóc khách hàng: Gọi tổng đài Shopee (miễn phí) hoặc Trò chuyện với Shopee ngay trên Trung tâm trợ giúp
+          </div>
+          <div className='mt-2'>Chịu Trách Nhiệm Quản Lý Nội Dung: Nguyễn Bùi Anh Tuấn</div>
+          <div className='mt-2'>
+            Mã số doanh nghiệp: 0106773786 do Sở Kế hoạch và Đầu tư TP Hà Nội cấp lần đầu ngày 10/02/2015
+          </div>
+          <div className='mt-2'>© 2015 - Bản quyền thuộc về Công ty TNHH Shopee</div>
+        </div>
+      </div>
+    </footer>
+  )
+}
